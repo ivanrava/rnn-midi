b5c95ccae95ddea2326daa0f6de30f0dbf03cd92
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-08-07 23:37:39 +0200

    Adds notewise dataloader implementation

--- dataloader.py
+++ dataloader.py
@@ -18,18 +18,38 @@ def set_seed(seed = 1337):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
 
-class MidiDataset(Dataset):
-    def __init__(self, midi_files, window_len):
-        self._midi_files = midi_files
+class NotewiseDataset(Dataset):
+    def __init__(self, midi_files: dict, window_len, notes_to_guess=1):
         self._window_len = window_len
+        self._notes_to_guess = notes_to_guess
+        # TODO: add genre
+        word_list = []
+        for genre_docs_list in midi_files.values():
+            for doc in genre_docs_list:
+                for word in doc:
+                    word_list.append(self.convert_word(word))
+
+        word_list = torch.tensor(word_list, dtype=torch.int64, device=torch.device('cpu'))
+        self.tensors = word_list.unfold(0, self._window_len, 1)
 
     def __len__(self):
-        return len(self._midi_files)
+        return len(self.tensors)
 
     def __getitem__(self, idx):
-        ...
+        designed_window = self.tensors[idx]
+        return designed_window[:self._window_len], designed_window[self._window_len:]
 
-def build_dataloader(dataset: MidiDataset, batch_size=16) -> DataLoader:
+    @staticmethod
+    def convert_word(word: str):
+        if word.startswith('p'):
+            return int(word.lstrip('p'))
+        elif word.startswith('endp'):
+            return int(word.lstrip('endp'))
+        else:
+            return int(word.lstrip('wait'))
+
+
+def build_dataloader(dataset: NotewiseDataset, batch_size=16) -> DataLoader:
     return DataLoader(dataset, batch_size=batch_size)
 
 def log(message):
@@ -58,3 +78,7 @@ if __name__ == '__main__':
     docs = read_documents("texts-4", '.notewise')
     log(f"Documents read: {sum([len(docs[g]) for g in docs])} totalling {sum([sum([sys.getsizeof(d) for d in docs[g]]) for g in docs]) / 10**6 } MB")
     log(f'Genres: {len(docs)}')
+
+    log("Building dataloader...")
+    dataloader = build_dataloader(NotewiseDataset(docs, 10), batch_size=16)
+    log("Dataloader built")
