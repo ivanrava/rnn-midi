93e1f38b54423de00282343b084c64cc09e9c1ec
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-08-08 14:27:01 +0200

    Lazy word conversion

--- dataloader.py
+++ dataloader.py
@@ -24,26 +24,27 @@ class NotewiseDataset(Dataset):
         self._notes_to_guess = notes_to_guess
         # TODO: add genre
         log("Reading words...")
-        word_list = []
+        self.word_list = []
         for genre_docs_list in midi_files.values():
             for doc in genre_docs_list:
                 for word in doc.split(' '):
                     if word == '':
                         continue
-                    word_list.append(word)
+                    self.word_list.append(word)
 
-        unique_words = set(word_list)
-        log(f"Finished reading words: {len(word_list)} (unique {len(unique_words)})")
+        unique_words = set(self.word_list)
+        log(f"Finished reading words: {len(self.word_list)} (unique {len(unique_words)})")
 
-        min_wait = min([int(w.lstrip('wait')) for w in unique_words if w.startswith('wait')])
-        max_wait = max([int(w.lstrip('wait')) for w in unique_words if w.startswith('wait')])
-        min_note = min([int(w.lstrip('p')) for w in unique_words if w.startswith('p')])
-        max_note = max([int(w.lstrip('p')) for w in unique_words if w.startswith('p')])
-        log(f"Min wait: {min_wait}")
-        log(f"Max wait: {max_wait}")
-        log(f"Min note: {min_note}")
-        log(f"Max note: {max_note}")
+        self.min_wait = min([int(w.lstrip('wait')) for w in unique_words if w.startswith('wait')])
+        self.max_wait = max([int(w.lstrip('wait')) for w in unique_words if w.startswith('wait')])
+        self.min_note = min([int(w.lstrip('p')) for w in unique_words if w.startswith('p')])
+        self.max_note = max([int(w.lstrip('p')) for w in unique_words if w.startswith('p')])
+        log(f"Min wait: {self.min_wait}")
+        log(f"Max wait: {self.max_wait}")
+        log(f"Min note: {self.min_note}")
+        log(f"Max note: {self.max_note}")
 
+        """
         log("Converting words into numbers...")
         word_list = [self.convert_word(word, max_wait, max_note, min_wait, min_note) for word in word_list]
         word_list = torch.tensor(word_list, dtype=torch.int64, device=torch.device('cpu'))
@@ -51,29 +52,37 @@ class NotewiseDataset(Dataset):
         log(f"Unfolding into {self._window_len} words windows...")
         self.tensors = word_list.unfold(0, self._window_len, 1)
         log("Unfolded / windowed!")
+        """
 
     def __len__(self):
-        return len(self.tensors)
+        return len(self.word_list) - self._window_len + 1
 
     def __getitem__(self, idx):
-        designed_window = self.tensors[idx]
-        return designed_window[:self._window_len-self._notes_to_guess], designed_window[self._window_len-self._notes_to_guess:]
+        designed_window = self.word_list[idx:idx + self._window_len]
+        designed_window = [self.convert_word(word) for word in designed_window]
 
-    @staticmethod
-    def convert_word(word: str, max_wait: int, max_note: int, min_wait, min_note: int):
-        wait_slice = np.zeros(max_wait - min_wait + 1)
-        note_slice = np.zeros(max_note - min_note + 1)
+        example = designed_window[:-self._notes_to_guess]
+        label = designed_window[self._window_len-self._notes_to_guess:]
+
+        example = torch.tensor(example, dtype=torch.int64, device=torch.device('cpu'))
+        label = torch.tensor(label, dtype=torch.int64, device=torch.device('cpu'))
+
+        return example, label
+
+    def convert_word(self, word: str):
+        wait_slice = np.zeros(self.max_wait - self.min_wait + 1)
+        note_slice = np.zeros(self.max_note - self.min_note + 1)
         if word.startswith('p'):
             note_index = int(word.lstrip('p'))
-            note_slice[note_index - min_note] = 1
+            note_slice[note_index - self.min_note] = 1
         elif word.startswith('endp'):
             note_index = int(word.lstrip('endp'))
-            note_slice[note_index - min_note] = -1
+            note_slice[note_index - self.min_note] = -1
         elif word == 'wait':
             raise Exception("Unexpected word")
         elif word.startswith('wait'):
             wait_index = int(word.lstrip('wait'))
-            wait_slice[wait_index - min_wait] = 1
+            wait_slice[wait_index - self.min_wait] = 1
         return np.concatenate((wait_slice, note_slice), axis=0)
 
 
